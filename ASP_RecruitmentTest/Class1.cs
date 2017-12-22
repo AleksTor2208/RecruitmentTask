393389ae33e4460f5f3ad71e04906c00276aaa4e
AleksTor2208 <aleksander.toropowski@gmail.com>
2017-12-22 20:30:36 +0100

    first commit

---
+++ ASP_RecruitmentTest/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP_RecruitmentTest
+{
+    public class Class1
+    {
+    }
+}
