5431eb0c4e9f52225b39e493d584640f65560130
AleksTor2208 <aleksander.toropowski@gmail.com>
2017-12-23 04:20:08 +0100

    Add CarRefuelingCalculatorTest testing diff pumps values with constant car array

---
+++ ASP_RecruitmentTest/CarRefuelingCalculatorTest.cs
@@ -0,0 +1,68 @@
+﻿using ASP_recruitment;
+using NUnit.Framework;
+
+namespace ASP_RecruitmentTest
+{
+	[TestFixture]
+	public class CarRefuelingCalculatorTest
+	{
+		readonly int[] cars = { 1, 4, 7, 2, 10, 15, 8 };
+
+		[Test]
+		public void TimeCountMethod_ReturnProperValueWhenTwoPumpsPassed()
+		{
+			//Arrange
+			var pumps = 2;
+			var expected = "25";
+
+			// Act
+			var actual = CarRefuelingCalculator.TimeCount(cars, pumps);
+
+			//Assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void TimeCountMethod_ReturnProperValueWhenThreePumpsPassed()
+		{
+			//Arrange
+			var pumps = 3;
+			var expected = "17";
+
+			// Act
+			var actual = CarRefuelingCalculator.TimeCount(cars, pumps);
+
+			//Assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void TimeCountMethod_ReturnProperValueWhenFourPumpsPassed()
+		{
+			//Arrange
+			var pumps = 4;
+			var expected = "15";
+
+			// Act
+			var actual = CarRefuelingCalculator.TimeCount(cars, pumps);
+
+			//Assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void TimeCountMethod_ReturnProperValueWhenFivePumpsPassed()
+		{
+			//Arrange
+			var pumps = 5;
+			var expected = "15";
+
+			// Act
+			var actual = CarRefuelingCalculator.TimeCount(cars, pumps);
+
+			//Assert
+			Assert.AreEqual(expected, actual);
+		}
+
+	}
+}
