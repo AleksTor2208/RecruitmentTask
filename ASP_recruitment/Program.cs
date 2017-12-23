c5a91b34f3c29739fb56d0d9af372813fe083b8e
AleksTor2208 <aleksander.toropowski@gmail.com>
2017-12-23 04:26:59 +0100

    Add Main method in Program.cs file

--- ASP_recruitment/Program.cs
+++ ASP_recruitment/Program.cs
@@ -10,6 +10,17 @@ namespace ASP_recruitment
     {
         static void Main(string[] args)
         {
-        }
-    }
+			int[] cars = { 1, 4, 7, 2, 10, 15, 8 };
+			var pumps = 6;
+			var task1 = CarRefuelingCalculator.TimeCount(cars, pumps);	
+			var task2 = SortingAlgorithm.SortStrByDigits("wyraz124 wyraz335 wy0raz 654wyraz 2wyrazy");
+			var task3 = IntToHexConverter.ConvertToHexadecimal(125, 125, 125);
+			Console.WriteLine(task1);
+			Console.WriteLine(task2);
+			Console.WriteLine(task3);
+			Console.Read();
+
+
+		}
+	}
 }
