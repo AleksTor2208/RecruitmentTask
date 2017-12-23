3d132d65f8c1d9b49289817fafba5a849459966f
AleksTor2208 <aleksander.toropowski@gmail.com>
2017-12-23 04:16:12 +0100

    Add tests for SortingAlgorithm class

---
+++ ASP_RecruitmentTest/SortingAlgorithmTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using ASP_recruitment;
+
+namespace ASP_RecruitmentTest
+{
+	[TestFixture]
+	public class SortingAlgorithmTest
+	{
+		[Test]
+		public void SortElementsMethod_ReturnProperValue()
+		{
+			//Arrange
+			var unsortedVal = "wyraz12 wyraz3 wy21raz";
+			var expected = "wyraz3 wyraz12 wy21raz";
+
+			// Act
+			var actual = SortingAlgorithm.SortElements(unsortedVal);
+			
+			//Assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void SortElementsMethod_ReturnProperValueWithArgHave5Words()
+		{
+			//Arrange
+			var unsortedVal = "wyraz124 wyraz335 wy0raz 654wyraz 2wyrazy";
+			var expected = "wy0raz 2wyrazy wyraz124 wyraz335 654wyraz";
+
+			// Act
+			var actual = SortingAlgorithm.SortElements(unsortedVal);
+
+			//Assert
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
