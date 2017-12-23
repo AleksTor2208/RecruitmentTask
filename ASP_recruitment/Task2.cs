671735ce04be7edb09dc72b6f1e9cd8d2c92f800
AleksTor2208 <aleksander.toropowski@gmail.com>
2017-12-23 01:41:19 +0100

    Add Task2 class with the solution of sorting method by digits taken from string input

---
+++ ASP_recruitment/Task2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ASP_recruitment
+{
+	internal class Task2
+	{
+		public static string SortElements(string stringInput)
+		{
+			var inputArray = stringInput.Split(' ');
+			var outputList = new List<string>();
+			var maxValue = int.MaxValue;
+			for (int i = 0; i < inputArray.Length; i++)
+			{
+				var digitValue = GetDigitValue(inputArray[i]);
+				if (digitValue < maxValue)
+				{
+					maxValue = digitValue;
+					outputList.Insert(0, inputArray[i]);
+				}
+				else
+				{
+					outputList.Add(inputArray[i]);
+				}						
+			}
+			return string.Join(" ", outputList.ToArray());
+		}
+
+		private static int GetDigitValue(string word)
+		{
+			var strBuilder = new StringBuilder();
+			for (int i = 0; i < word.Length; i++)
+			{
+				if (char.IsDigit(word[i]))
+				{
+					strBuilder.Append(word[i]);
+				}
+			}
+			return int.Parse(strBuilder.ToString());
+		}
+	}
+}
